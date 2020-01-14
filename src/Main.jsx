a3a6909964f2d946db472bc6c9d7adc64081023e
Martin Cajthaml <martin.cajthaml@symbio.agency>
2020-01-14 13:55:01 +0100

    rewrite

--- src/Main.jsx
+++ src/Main.jsx
@@ -16,9 +16,7 @@ import './style.css';
 }))
 export default class Main extends Component {
   static propTypes = {
-    token: PropTypes.string,
     maxRecords: PropTypes.number,
-    itemId: PropTypes.string,
     fieldPath: PropTypes.string,
     getFieldValue: PropTypes.func,
     setFieldValue: PropTypes.func,
@@ -28,68 +26,80 @@ export default class Main extends Component {
   state = {
     loading: true,
     dropDown: false,
-    data: {},
+    data: [],
     values: [],
   };
 
   componentDidMount() {
-    const { addFieldChangeListener } = this.props;
+    const { addFieldChangeListener, getFieldValue, fieldPath } = this.props;
 
-    this.updateData();
+    this.setState({
+      values: getFieldValue(fieldPath),
+    });
 
-    addFieldChangeListener('subtitle_files_languages', () => {
+    addFieldChangeListener('subtitle_files', () => {
       this.updateData();
     });
   }
 
-  getLanguageItem(itemRow) {
+  getLanguageItem(item) {
     const { setFieldValue, fieldPath } = this.props;
-    const { data, values } = this.state;
+    const { values } = this.state;
 
-    const subt = data.subtitleFilesLanguages.find(subtitle => (
-      subtitle.id.uploadId === itemRow
-    ));
-    const locale = subt ? subt.lang : null;
-
-    const languageName = this.detectLanguageFromLocale(locale);
+    const languageName = this.detectLanguageFromLocale(item);
 
     return (
-      <div className="language-box" key={`subtitle_${itemRow}`} id={`subtitle_${itemRow}`}>
-        <div>{`${languageName || locale} [${itemRow}]`}</div>
+      <div
+        className="language-box"
+        key={`subtitle_${item}`}
+        id={`subtitle_${item}`}
+      >
+        <div>{`${languageName || item}`}</div>
         <button
           type="button"
           className="cross"
           onClick={() => {
-            values.splice(values.indexOf(itemRow), 1);
-            setFieldValue(fieldPath, JSON.stringify(values));
+            const newValues = values.filter(v => v !== item);
+            this.setState({
+              values: newValues,
+            });
+            setFieldValue(fieldPath, JSON.stringify(newValues));
           }}
         >
-          <svg className="svg-cross" height="14" width="14" viewBox="0 0 20 20" aria-hidden="true" focusable="false"><path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z" /></svg>
+          <svg
+            className="svg-cross"
+            height="14"
+            width="14"
+            viewBox="0 0 20 20"
+            aria-hidden="true"
+            focusable="false"
+          >
+            <path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z" />
+          </svg>
         </button>
       </div>
     );
   }
 
-  getLanguageOption(dataRow) {
+  getLanguageOption(lang) {
     const { setFieldValue, fieldPath } = this.props;
     const { values } = this.state;
 
-    const languageName = this.detectLanguageFromLocale(dataRow.lang);
+    const languageName = this.detectLanguageFromLocale(lang);
 
     return (
       <li
-        key={`dropDownItem_${dataRow.id.uploadId}`}
+        key={`dropDownItem_${lang}`}
         className="dropDownItem"
         onClick={() => {
-          values.push(dataRow.id.uploadId);
-          setFieldValue(fieldPath, JSON.stringify(values));
+          const newValues = [...values, lang];
+          setFieldValue(fieldPath, JSON.stringify(newValues));
+          this.setState({
+            values: newValues,
+          });
         }}
       >
-        {`${languageName || dataRow.lang}`}
-        <small>
-          Titulky (TIT soubory) #
-          {dataRow.id.uploadId}
-        </small>
+        {`${languageName || lang}`}
       </li>
     );
   }
@@ -127,8 +137,12 @@ export default class Main extends Component {
         event.relatedTarget.classList.toggle('can-drop');
       },
       ondrop(event) {
-        const dropzoneArrayIndex = values.indexOf(event.target.id.split('_')[1]);
-        const draggableArrayIndex = values.indexOf(event.relatedTarget.id.split('_')[1]);
+        const dropzoneArrayIndex = values.indexOf(
+          event.target.id.split('_')[1],
+        );
+        const draggableArrayIndex = values.indexOf(
+          event.relatedTarget.id.split('_')[1],
+        );
 
         const removedValue = values.splice(
           dropzoneArrayIndex,
@@ -139,6 +153,9 @@ export default class Main extends Component {
 
         event.relatedTarget.classList.toggle('can-drop');
         setFieldValue(fieldPath, JSON.stringify(values));
+        this.setState({
+          values,
+        });
       },
       ondropdeactivate(event) {
         const e = event;
@@ -172,52 +189,40 @@ export default class Main extends Component {
     });
   }
 
-  updateData() {
-    const { token, itemId } = this.props;
-    const { getFieldValue, fieldPath } = this.props;
+  async updateData() {
+    const { getFieldValue, setFieldValue, fieldPath } = this.props;
 
+    const files = getFieldValue('subtitle_files');
+    const values = JSON.parse(getFieldValue(fieldPath));
+
+    const { data } = await fetch(
+      'https://nd-test.symbio.now.sh/api/subtitles/getSubtitlesLanguages',
+      {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(files.map(f => f.uploadId)),
+      },
+    );
+
+    // filter out invalid languages
+    const langs = data.map(d => d.lang);
+
+    const newValues = values.filter(v => langs.indexOf(v) !== -1);
+    setFieldValue(fieldPath, JSON.stringify(newValues));
     this.setState({
-      loading: true,
+      data: langs,
+      values: newValues,
     });
 
-    fetch('https://graphql.datocms.com/preview', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({
-        query: `
-        {
-          production(filter: {id: {eq: "${itemId}"}}) {
-            subtitleFilesLanguages
-          }
-        }
-        `,
-      }),
-    })
-      .then(res => res.json())
-      .then((res) => {
-        this.setState({
-          loading: false,
-          data: res.data.production,
-          values: JSON.parse(getFieldValue(fieldPath)) || [],
-        });
-        this.initializeInteract();
-      })
-      .catch((error) => {
-        this.setState({
-          loading: false,
-        });
-        console.log(error);
-      });
+    this.initializeInteract();
   }
 
   render() {
-    const {
-      loading, dropDown, data, values,
-    } = this.state;
+    const { loading, dropDown, values, data } = this.state;
     const { maxRecords } = this.props;
 
     if (loading) {
@@ -230,38 +235,61 @@ export default class Main extends Component {
           role="button"
           tabIndex="0"
           className="select-container"
-          onClick={(e) => { this.toggleDropDown(e); }}
-          onKeyDown={(e) => { this.toggleDropDown(e); }}
+          onClick={e => {
+            this.toggleDropDown(e);
+          }}
+          onKeyDown={e => {
+            this.toggleDropDown(e);
+          }}
         >
-          {Array.isArray(values) && values.length !== 0 ? values.map(subtitleId => (
-            this.getLanguageItem(subtitleId)
-          )) : (
-            <span onClick={(e) => { this.toggleDropDown(e); }} role="button" tabIndex="0">
+          {Array.isArray(values) && values.length > 0 ? (
+            values.map(lang => this.getLanguageItem(lang))
+          ) : (
+            <span
+              onClick={e => {
+                this.toggleDropDown(e);
+              }}
+              role="button"
+              tabIndex="0"
+            >
               Vyberte maximálně 2 jazyky...
             </span>
-          )
-          }
-          <div onClick={(e) => { this.toggleDropDown(e); }} aria-hidden="true" className="dropDownArrow"><svg onClick={(e) => { this.toggleDropDown(e); }} height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false" className="css-19bqh2r"><path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z" /></svg></div>
-        </div>
-        {dropDown
-          && (
-            <ul className="dropDown">
-              {!Array.isArray(values) || values.length < maxRecords
-                ? Array.isArray(data.subtitleFilesLanguages)
-                && data.subtitleFilesLanguages.length !== 0
-                  ? data.subtitleFilesLanguages.map(dataRow => (
-                    this.getLanguageOption(dataRow)
-                  )) : <div>Žádné titulky nejsou k dispozici...</div>
-                : (
-                  <div>
-                    Vybrán maximální počet titulků (
-                    {maxRecords}
-                    )...
-                  </div>
-                )
-              }
-            </ul>
           )}
+          <div
+            onClick={e => {
+              this.toggleDropDown(e);
+            }}
+            aria-hidden="true"
+            className="dropDownArrow"
+          >
+            <svg
+              onClick={e => {
+                this.toggleDropDown(e);
+              }}
+              height="20"
+              width="20"
+              viewBox="0 0 20 20"
+              aria-hidden="true"
+              focusable="false"
+              className="css-19bqh2r"
+            >
+              <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z" />
+            </svg>
+          </div>
+        </div>
+        {dropDown && (
+          <ul className="dropDown">
+            {!Array.isArray(values) || values.length < maxRecords ? (
+              Array.isArray(data) && data.length > 0 ? (
+                data.map(lang => this.getLanguageOption(lang))
+              ) : (
+                <div>Žádné titulky nejsou k dispozici...</div>
+              )
+            ) : (
+              <div>Vybrán maximální počet titulků ({maxRecords})...</div>
+            )}
+          </ul>
+        )}
       </div>
     );
   }
